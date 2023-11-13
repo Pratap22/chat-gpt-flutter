aacd5d3e19b4b2ecc42c33a014e2e37f511fa6a8
Pratap Sharma <prs@logpoint.com>
2023-11-13 22:46:53 +0545

    Completed the app

---
+++ lib/providers/models_provider.dart
@@ -0,0 +1,27 @@
+
+import 'package:chat_gpt/models/models_model.dart';
+import 'package:chat_gpt/services/api_service.dart';
+import 'package:flutter/cupertino.dart';
+
+class ModelsProvider with ChangeNotifier {
+  // String currentModel = "text-davinci-003";
+  String currentModel = "gpt-3.5-turbo-0301";
+
+  String get getCurrentModel => currentModel;
+
+  void setCurrentModel(String newModel) {
+    currentModel = newModel;
+    notifyListeners();
+  }
+
+  List<ModelsModel> modelsList = [];
+
+  List<ModelsModel> get getModelsList {
+    return modelsList;
+  }
+
+  Future<List<ModelsModel>> getAllModels() async {
+    modelsList = await ApiService.getModels();
+    return modelsList;
+  }
+}
